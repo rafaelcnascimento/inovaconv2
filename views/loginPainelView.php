1ebf3fd0d39b25e452aae97ed6c17b0084c0a697
Rafael <rafaelcatelandonascimento@gmail.com>
2018-06-19 10:58:00 -0300

    kraft2

--- views/loginPainelView.php
+++ views/loginPainelView.php
@@ -4,7 +4,7 @@
     <!-- Mobile Specific Meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon-->
-    <link rel="shortcut icon" href="../assets/img/favicon.png">
+    <link rel="shortcut icon" href="../ejr/assets/img/favicon.png">
     <!-- Author Meta -->
     <meta name="author" content="ADS">
     <!-- Idioma -->
@@ -22,21 +22,21 @@
         <!--
         CSS
         ============================================= -->
-        <link rel="stylesheet" href="../assets/css/linearicons.css">
-        <link rel="stylesheet" href="../assets/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../ejr/assets/css/linearicons.css">
+        <link rel="stylesheet" href="../ejr/assets/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../assets/css/magnific-popup.css">
-        <link rel="stylesheet" href="../assets/css/nice-select.css">                          
-        <link rel="stylesheet" href="../assets/css/animate.min.css">
-        <link rel="stylesheet" href="../assets/css/owl.carousel.css">
-        <link rel="stylesheet" href="../assets/css/main.css">
+        <link rel="stylesheet" href="../ejr/assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="../ejr/assets/css/nice-select.css">                          
+        <link rel="stylesheet" href="../ejr/assets/css/animate.min.css">
+        <link rel="stylesheet" href="../ejr/assets/css/owl.carousel.css">
+        <link rel="stylesheet" href="../ejr/assets/css/main.css">
         <!--Nosso CSS personalizado vai aqui-->
-        <link rel="stylesheet" href="../assets/css/personalizado.css">
+        <link rel="stylesheet" href="../ejr/assets/css/personalizado.css">
     </head>
 <body>
     <div class="container" style="margin-top: 10%">
         <center>
-            <img src="../assets/img/logo.png">
+            <img src="../ejr/assets/img/logo.png">
             <div class="panel panel-primary" style="width: 500px">
                 <div class="panel-heading">
                     <h3 class="panel-title">Área restrita</h3>
